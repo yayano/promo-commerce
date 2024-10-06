d71046723d7bbe13d0b5875b4b43d7a0125547f6
yayano <boussetta13yahia@gmail.com>
2024-10-06 21:42:14 +0100

    home latest collection &  best seller

--- frontEnd/src/components/Hero.jsx
+++ frontEnd/src/components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
             <p className="w-8 md:w-11 h-[2px] bg-[#414141]"></p>
             <p className="font-medium text-sm md:text-base ">OUR BESTSELLERS</p>
           </div>
-          <h1 className="text-3xl sm:py-3 lg:text-5xl leading-relaxed">
+          <h1 className="text-3xl sm:py-3 lg:text-5xl leading-relaxed prata-regular">
             Latest Arrivals
           </h1>
           <div className="flex items-center gap-2">
