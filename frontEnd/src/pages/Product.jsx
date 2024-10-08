e58044cdfffd5ee3edf05ddd160712f15ccf4cd8
yayano <boussetta13yahia@gmail.com>
2024-10-08 15:59:05 +0100

    product details

--- frontEnd/src/pages/Product.jsx
+++ frontEnd/src/pages/Product.jsx
@@ -1,7 +1,56 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { ShopContext } from "../context/ShopContext";
 
 const Product = () => {
-  return <div>Product</div>;
+  const { productId } = useParams();
+  const { products } = useContext(ShopContext);
+  const [productData, setProductData] = useState(false);
+  const [image, setImage] = useState("");
+
+  const fetchProductData = async () => {
+    products.map((item) => {
+      if (item._id === productId) {
+        setProductData(item);
+        setImage(item.image[0]);
+        return;
+      }
+    });
+  };
+  useEffect(() => {
+    fetchProductData();
+  }, [productId, products]);
+
+  return productData ? (
+    <div className="border-t-2 pt-10 transition-opacity ease-in duration-500 opacity-100">
+      {/**Products Data */}
+      <div className="flex gap-12 sm:gap-12 flex-col sm:flex-row">
+        {/**Product Images */}
+        <div className="flex-1 flex flex-col-reverse gap-3 sm:flex-row">
+          <div className="flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full">
+            {productData.image.map((item, index) => (
+              <img
+                src={item}
+                key={index}
+                className="w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer "
+                alt=""
+                onMouseEnter={() => setImage(item)}
+              />
+            ))}
+          </div>
+          <div className="w-full sm:w-[80%]">
+            <img src={image} className="w-full h-auto" alt="" />
+          </div>
+        </div>
+        {/** ----------------Product Info */}
+        <div className="flex-1 ">
+          <h1> {productData.name} </h1>
+        </div>
+      </div>
+    </div>
+  ) : (
+    <div className="opacity-0"></div>
+  );
 };
 
 export default Product;
