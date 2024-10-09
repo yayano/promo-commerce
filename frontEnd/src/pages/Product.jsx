f35d25491344480fb2c60359dcd82b93edd5be25
yayano <boussetta13yahia@gmail.com>
2024-10-09 23:57:32 +0200

    products details end

--- frontEnd/src/pages/Product.jsx
+++ frontEnd/src/pages/Product.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { ShopContext } from "../context/ShopContext";
 import { assets } from "../assets/assets";
+import RelatedProducts from "../components/RelatedProducts";
 
 const Product = () => {
   const { productId } = useParams();
@@ -112,6 +113,10 @@ const Product = () => {
         </div>
       </div>
       {/**Display related products --- */}
+      <RelatedProducts
+        category={productData.category}
+        subCategory={productData.subCategory}
+      />
     </div>
   ) : (
     <div className="opacity-0"></div>
