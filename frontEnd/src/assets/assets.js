1ab047beb853d51bfe2a58c7f9175364d845467d
yayano <boussetta13yahia@gmail.com>
2024-10-11 15:51:01 +0100

    add products

--- frontEnd/src/assets/assets.js
+++ frontEnd/src/assets/assets.js
@@ -85,6 +85,13 @@ import img_9_3 from "./promoPhone/28.jpg";
 import img_10_1 from "./promoPhone/29.jpg";
 import img_10_2 from "./promoPhone/30.jpg";
 import img_11 from "./promoPhone/31.jpg";
+import img_12 from "./promoPhone/32.jpg";
+import img_12_1 from "./promoPhone/33.jpg";
+import img_13 from "./promoPhone/34.jpg";
+import img_13_1 from "./promoPhone/35.jpg";
+import img_13_2 from "./promoPhone/36.jpg";
+import img_13_3 from "./promoPhone/37.jpg";
+import img_13_4 from "./promoPhone/38.jpg";
 
 import logo from "./promoLogo.png";
 import hero_img from "./hero_section.png";
@@ -298,6 +305,33 @@ export const products = [
     sizes: ["S", "M", "L", "XL"],
     date: 1716625545448,
     bestseller: false,
+  },
+
+  {
+    _id: "aaaac",
+    name: "Hoco Fontaine Q20",
+    description:
+      "Hoco Fontaine Q20, power banque d'alimentation mobile, 10000mAh, entrée Type-C, port Type-C unique et câbles intégrés pour sorties Lightning Iphone et Type-C, prend en charge les protocoles de charge rapide NOIR",
+    price: 3130,
+    image: [img_12, img_12_1],
+    category: "Accessoires",
+    subCategory: "Confort",
+    sizes: ["S", "L", "XL"],
+    date: 1716234545448,
+    bestseller: true,
+  },
+  {
+    _id: "aaaac1",
+    name: "XIAOMI Kieslect SMART CALLING WATCH KR 2",
+    description:
+      "Kieslect Montre intelligente Calling Watch KR 2Grâce à la conception robuste du haut-parleur et du microphone, le Kieslect Smart Calling KR 2 permet des appels Bluetooth clairs et de haute qualité partout et à tout moment !La Smart Watch KIESLECT KR 2 est votre assistant personnel intelligent et polyvalent. Il dispose d'une autonomie de batterie jusqu'à 5 jours, d'une fonction de monitoring cardiaque, de suivi d'activité et d'un moniteur de sommeil. Il est compatible avec iOS et Android et peut être synchronisé avec votre téléphone.",
+    price: 12700,
+    image: [img_13, img_13_1, img_13_2, img_13_3, img_13_4],
+    category: "Accessoires",
+    subCategory: "Confort",
+    sizes: ["S", "L", "XL"],
+    date: 1716234545448,
+    bestseller: true,
   },
 
   {
