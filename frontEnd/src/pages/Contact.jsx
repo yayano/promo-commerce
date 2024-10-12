2c7d3eea27deb8db223f68e79fa12dd1ac8addfd
yayano <boussetta13yahia@gmail.com>
2024-10-12 17:14:34 +0100

    contact

--- frontEnd/src/pages/Contact.jsx
+++ frontEnd/src/pages/Contact.jsx
@@ -1,7 +1,42 @@
 import React from "react";
-
+import { assets } from "../assets/assets";
+import Title from "../components/Title";
+import NewsLetterBox from "../components/NewsLetterBox";
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div>
+      <div className="text-2xl text-center pt-8 border-t">
+        <Title text1={"CONTACT"} text2={"US"} />
+      </div>
+      <div className="my-10 flex flex-col justify-center md:flex-row gap-10 mb-28">
+        <img
+          className="w-full md:max-w-[480px]"
+          src={assets.contact_img}
+          alt=""
+        />
+        <div className="flex flex-col justify-center items-start gap-6  ">
+          <p className="font-semibold text-xl text-gray-600 ">Our Store</p>
+          <p className="text-gray-500">
+            Boufarik City <br /> Suite 350 , Boufatik dummy ...
+          </p>
+          <p className="text-gray-500">
+            Tel: (+213) 540-415-927 <br /> Email: Boussetta13yahia@gmail.com
+          </p>
+          <p className="font-semibold text-xl text-gray-500">
+            {" "}
+            Careers at PROMO-PHONE
+          </p>
+          <p className="text-gray-500">
+            Learn more about aour teams and job opening.
+          </p>
+          <button className="border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500">
+            Explore Jobs
+          </button>
+        </div>
+      </div>
+      <NewsLetterBox />
+    </div>
+  );
 };
 
 export default Contact;
