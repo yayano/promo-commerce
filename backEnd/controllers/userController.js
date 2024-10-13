0228dc39489c03349bd7e36a7bc7ff2ee4e0aeae
yayano <boussetta13yahia@gmail.com>
2024-10-13 20:06:58 +0100

    user controller functions

---
+++ backEnd/controllers/userController.js
@@ -0,0 +1,74 @@
+import userModel from "../models/userModel.js";
+import validator from "validator";
+import bcrybt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const createToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET);
+};
+//route for user login
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      return res.json({ success: false, message: "user doesn't exists" });
+    }
+    const isMatch = await bcrybt.compare(password, user.password);
+    if (isMatch) {
+      const token = createToken(user._id);
+      res.json({ success: true, token });
+    } else {
+      res.json({ success: false, message: "invalid credentials" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+//route for user register
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    //checking if email already exist
+    const exists = await userModel.findOne({ email });
+    if (exists) {
+      return res.json({ success: false, message: "user already exists" });
+    }
+    //validating email format and password strong
+    if (!validator.isEmail(email)) {
+      return res.json({
+        success: false,
+        message: "please enter a valid Email",
+      });
+    }
+    if (password.length < 8) {
+      return res.json({
+        success: false,
+        message: "please enter a strong password",
+      });
+    }
+    //hashing user password
+    const salt = await bcrybt.genSalt(10);
+    const hashedPassword = await bcrybt.hash(password, salt);
+
+    const newUser = new userModel({
+      name,
+      email,
+      password: hashedPassword,
+    });
+    console.log("user", newUser.hash);
+    const user = await newUser.save();
+    const token = createToken(user._id);
+    res.json({ success: true, token });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+//route for admin login
+const adminLogin = async (req, res) => {};
+
+export { loginUser, registerUser, adminLogin };
