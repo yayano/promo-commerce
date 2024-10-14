cfd0ff2139f32ff1d38d0b9c0d730c3e3f805112
yayano <boussetta13yahia@gmail.com>
2024-10-14 16:52:43 +0100

    backend end

--- backEnd/routes/productRoute.js
+++ backEnd/routes/productRoute.js
@@ -6,6 +6,7 @@ import {
   singleProduct,
 } from "../controllers/productController.js";
 import upload from "../middleware/multer.js";
+import adminAuth from "../middleware/adminAuth.js";
 const productRouter = express.Router();
 const fields = [
   { name: "image1", maxCount: 1 },
@@ -13,8 +14,8 @@ const fields = [
   { name: "image3", maxCount: 1 },
   { name: "image4", maxCount: 1 },
 ];
-productRouter.post("/add", upload.fields(fields), addProduct);
-productRouter.post("/remove", removeProducts);
+productRouter.post("/add", adminAuth, upload.fields(fields), addProduct);
+productRouter.post("/remove", adminAuth, removeProducts);
 productRouter.post("/single", singleProduct);
 productRouter.get("/list", listProducts);
 
