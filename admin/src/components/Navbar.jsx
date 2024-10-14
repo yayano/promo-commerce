45e7dd85508910caeadf92aed772d0d19114a78d
yayano <boussetta13yahia@gmail.com>
2024-10-14 18:31:52 +0100

    admin panel navbar & sidebar

---
+++ admin/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { assets } from "../assets/assets";
+const Navbar = () => {
+  return (
+    <div className="flex items-center justify-between px-[4%] py-2">
+      <img className="w-[max(10%,80px)] py-1 px-1" src={assets.logo} alt="" />
+      <button className="bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm">
+        Logout
+      </button>
+    </div>
+  );
+};
+
+export default Navbar;
