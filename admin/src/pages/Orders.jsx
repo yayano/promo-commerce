e3d5466401bd779fc70c4842affdae4000eeae27
yayano <boussetta13yahia@gmail.com>
2024-10-21 15:14:53 +0200

    reload aorders

--- admin/src/pages/Orders.jsx
+++ admin/src/pages/Orders.jsx
@@ -1,6 +1,33 @@
 import React from "react";
-
-const Orders = () => {
+import { useEffect } from "react";
+import { useState } from "react";
+import axios from "axios";
+import { backendUrl } from "../App";
+const Orders = ({ token }) => {
+  const [orders, setOrders] = useState([]);
+  const fetchAllOrders = async () => {
+    if (!token) {
+      return null;
+    }
+    try {
+      const response = await axios.post(
+        backendUrl + "/api/order/list",
+        {},
+        { headers: { token } }
+      );
+      if (response.data.success) {
+        setOrders(response.data.orders);
+      } else {
+        toast.error(response.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+  };
+  useEffect(() => {
+    fetchAllOrders();
+  }, [token]);
   return <div>Orders</div>;
 };
 
