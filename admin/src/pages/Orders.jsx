b7a5efed27c3d47b5988fae88f16d716cde9a260
yayano <boussetta13yahia@gmail.com>
2024-10-22 19:30:42 +0100

    orders for admin panel

--- admin/src/pages/Orders.jsx
+++ admin/src/pages/Orders.jsx
@@ -2,8 +2,9 @@ import React from "react";
 import { useEffect } from "react";
 import { useState } from "react";
 import axios from "axios";
-import { backendUrl } from "../App";
+import { backendUrl, currency } from "../App";
 import { assets } from "../assets/assets";
+import { toast } from "react-toastify";
 const Orders = ({ token }) => {
   const [orders, setOrders] = useState([]);
   const fetchAllOrders = async () => {
@@ -26,6 +27,21 @@ const Orders = ({ token }) => {
       toast.error(error.message);
     }
   };
+  const statusHandler = async (event, orderId) => {
+    try {
+      const response = await axios.post(
+        backendUrl + "/api/order/status",
+        { orderId, status: event.target.value },
+        { headers: { token } }
+      );
+      if (response.data.success) {
+        await fetchAllOrders();
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(response.data.message);
+    }
+  };
   useEffect(() => {
     fetchAllOrders();
   }, [token]);
@@ -34,24 +50,70 @@ const Orders = ({ token }) => {
       <h3>Order Page</h3>
       <div>
         {orders.map((order, index) => (
-          <div key={index}>
-            <img src={assets.parcel_icon} alt="" />
+          <div
+            key={index}
+            className="grid grid-cols-1 sm:grid-cols-[0.5fr_2fr_1fr] lg:grid-cols-[0.5fr_2fr_1fr_1fr_1fr] gap-3 items-start border-2 border-gray-200 p-5 md:p-8 my-3 md:my-4 text-xs sm:text:sm text-gray-700 "
+          >
+            <img className="w-12" src={assets.parcel_icon} alt="" />
             <div>
-              {order.items.map((item, index) => {
-                if (index === order.items.length - 1) {
-                  return (
-                    <p key={index}>
-                      {item.name} x {item.quantity} <span> {item.size} </span>
-                    </p>
-                  );
-                } else {
-                  <p key={index}>
-                    {item.name} x {item.quantity} <span> {item.size} </span>,
-                  </p>;
-                }
-              })}
+              <div>
+                {order.items.map((item, index) => {
+                  if (index === order.items.length - 1) {
+                    return (
+                      <p className="py-0.5" key={index}>
+                        {item.name} x {item.quantity} <span> {item.size} </span>
+                      </p>
+                    );
+                  } else {
+                    <p className="py-0.5" key={index}>
+                      {item.name} x {item.quantity} <span> {item.size} </span>,
+                    </p>;
+                  }
+                })}
+              </div>
+              <p className="mt-3 mb-2 font-medium">
+                {" "}
+                {order.address.firstName + ", " + order.address.lastName}{" "}
+              </p>
+              <div>
+                <p> {order.address.street + ", "} </p>
+
+                <p>
+                  {" "}
+                  {order.address.city +
+                    ", " +
+                    order.address.state +
+                    ", " +
+                    order.address.country +
+                    ", " +
+                    order.address.zipCode}{" "}
+                </p>
+              </div>
+              <p>{order.address.phone}</p>
             </div>
-            <p> {order.address.firstName + "" + order.address.lastName} </p>
+            <div>
+              <p className="text-sm sm:text-[15px]">
+                Items : {order.items.length}
+              </p>
+              <p className="mt-3">Methode : {order.paymentMethod}</p>
+              <p>Payment : {order.payment ? "Done" : "Pending"} </p>
+              <p> Date : {new Date(order.data).toLocaleDateString()} </p>
+            </div>
+            <p className="text-sm sm:text-[15px]">
+              {" "}
+              {order.amount} {currency}
+            </p>
+            <select
+              onChange={(event) => statusHandler(event, order._id)}
+              value={order.status}
+              className="p-2 sont-semibold"
+            >
+              <option value="Order Placed">Order Placed</option>
+              <option value="Packing">Packing</option>
+              <option value="Shipped">Shipped</option>
+              <option value="Out dor delivery">Out dor delivery</option>
+              <option value="Delivered">Delivered</option>
+            </select>
           </div>
         ))}
       </div>
--- backEnd/controllers/orderController.js
+++ backEnd/controllers/orderController.js
@@ -52,7 +52,16 @@ const userOrders = async (req, res) => {
 };
 
 //update order status from admin panel
-const updateStatus = async (req, res) => {};
+const updateStatus = async (req, res) => {
+  try {
+    const { orderId, status } = req.body;
+    await orderModel.findByIdAndUpdate(orderId, { status });
+    res.json({ success: true, message: "Status Updated" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 export {
   placeOrder,
